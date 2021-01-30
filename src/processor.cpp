00f522e985a16b7a634c339a3f1149a48199a92f
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-01-30 12:59:35 +0100

    Make cpu utilization calculation more dynamic

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,10 +1,18 @@
 #include "processor.h"
 #include "linux_parser.h"
+#include <unistd.h>
 
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization() {
     long activeJiffies = LinuxParser::ActiveJiffies();
     long totalJiffies = LinuxParser::Jiffies();
+
+    int samplingPeriodSeconds = 1;
+    usleep(samplingPeriodSeconds * 1000000);
+
+    activeJiffies = LinuxParser::ActiveJiffies() - activeJiffies;
+    totalJiffies = LinuxParser::Jiffies() - totalJiffies;
+
     float utilization = (float) activeJiffies / (float) totalJiffies;
     return utilization; }
 
