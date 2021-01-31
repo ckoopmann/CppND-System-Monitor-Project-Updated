b0b7008a10ac12f36ff1781fac5410e4dfdc1a78
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-01-31 14:29:56 +0100

    Switch back to CPU Utilization calculation based on total Time

--- src/processor.cpp
+++ src/processor.cpp
@@ -7,12 +7,6 @@ float Processor::Utilization() {
     long activeJiffies = LinuxParser::ActiveJiffies();
     long totalJiffies = LinuxParser::Jiffies();
 
-    int samplingPeriodSeconds = 1;
-    usleep(samplingPeriodSeconds * 1000000);
-
-    activeJiffies = LinuxParser::ActiveJiffies() - activeJiffies;
-    totalJiffies = LinuxParser::Jiffies() - totalJiffies;
-
     float utilization = (float) activeJiffies / (float) totalJiffies;
     return utilization; }
 
